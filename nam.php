371093bca3cf944639644616cd40c415607d5495
namtdn <48233621+namtdn@users.noreply.github.com>
2019-03-22 11:45:42 +0700

    Update nam.php

--- nam.php
+++ nam.php
@@ -15,27 +15,32 @@
             return $firstNumber + $secondNumber;
         }
     }
-    
+
     class Minus implements Operator{
-    	public function operate(float $firstNumber, float $secondNumber){
-        	return $firstNumber - $secondNumber;
+        public function operate(float $firstNumber, float $secondNumber){
+            return $firstNumber - $secondNumber;
         }
     }
-    
+
     class Multiply implements Operator{
-    	public function operate(float $firstNumber, float $secondNumber){
-        	return $firstNumber * $secondNumber;
+        public function operate(float $firstNumber, float $secondNumber){
+            return $firstNumber * $secondNumber;
         }
     }
 
     class Divide implements Operator{
         public function operate(float $firstNumber, float $secondNumber){
-            if ($secondNumber === 0){
-                throw new Exception("Fail");
+        	try{
+            	if ($secondNumber == 0){
+                	throw new Exception("Fail");
+            	}
+            	else{
+                	return $firstNumber / $secondNumber;
+            	}
             }
-            else{
-                return $firstNumber / $secondNumber;
-            }
+            catch (Exception $e){
+        		echo $e->getMessage();
+    		}
         }
     }
 
@@ -44,11 +49,6 @@
     echo $calculator->calculate(new Add(), 5, 5)."<br>";
     echo $calculator->calculate(new Minus(), 5, 5)."<br>";
     echo $calculator->calculate(new Multiply(), 5, 5)."<br>";
+    echo $calculator->calculate(new Divide(), 5, 0);
 
-    try{
-        echo $calculator->calculate(new Divide(), 5, 5);
-    }
-    catch (Exception $e){
-        echo $e->getMessage();
-    }
 ?>
